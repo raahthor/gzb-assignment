1742a84de29cbdc91fa41efdb8ccded89773f228
Prashant <prashantrathore1435@gmail.com>
2025-05-29 18:33:39 +0530

    text alignments fixed

--- components/Description.tsx
+++ components/Description.tsx
@@ -123,7 +123,7 @@ const Detail = ({ label, value }: DetailProps) => (
     py={3}
   >
     <Text fontSize="md" color="gray.600" w="180px">{label}</Text>
-    <Box fontWeight="bold" color="black">
+    <Box fontWeight="bold" color="black" w="200px">
       {value}
     </Box>
   </HStack>
